e626dd5b4588bceb07dc977ccef0165dab863f82
josephdrose@gmail.com <josephdrose@gmail.com@1d1e89a0-1f47-3da9-675a-c49156f51798>
2010-12-03 00:06:38 +0000

    Missile tracking.

--- trunk/src/main/java/spacesim/Util.java
+++ trunk/src/main/java/spacesim/Util.java
@@ -4,4 +4,8 @@ public class Util {
 	public static double distance(double x1, double y1, double x2, double y2) {
 		return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 	}
+	
+	public static double angle(double x1, double y1, double x2, double y2) {
+		return Math.toDegrees(Math.atan2((y2-y1), (x2-x1)));
+	}
 }
