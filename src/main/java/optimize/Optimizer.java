fbd0802ae9ee0392ce0764a5540ca0d1b91bfdac
josephdrose@gmail.com <josephdrose@gmail.com@1d1e89a0-1f47-3da9-675a-c49156f51798>
2011-05-25 12:00:45 +0000

    Still needs scoring, but framework is in place.

--- src/main/java/optimize/Optimizer.java
+++ src/main/java/optimize/Optimizer.java
@@ -2,14 +2,14 @@ package optimize;
 
 import org.drools.planner.config.XmlSolverConfigurer;
 import org.drools.planner.core.Solver;
+import org.drools.planner.core.localsearch.DefaultLocalSearchSolver;
 
 public class Optimizer {
 	public static void main(String [] args) {
 		XmlSolverConfigurer configurer = new XmlSolverConfigurer(); 
 		configurer.configure("/optimize/config.xml"); 
 		Solver solver = configurer.buildSolver();
-		
-		//optimizing fire distance
+		((DefaultLocalSearchSolver) solver).setScoreCalculator(new SimulationScoreCalculator());
 		solver.setStartingSolution(new VariableSolution(new Variable(310)));
 		solver.solve(); 
 		VariableSolution bestSolution = (VariableSolution) solver.getBestSolution();
--- src/main/java/optimize/VariableMoveFactory.java
+++ src/main/java/optimize/VariableMoveFactory.java
@@ -3,44 +3,19 @@ package optimize;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.drools.planner.core.localsearch.LocalSearchSolverScope;
-import org.drools.planner.core.localsearch.StepScope;
-import org.drools.planner.core.localsearch.decider.Decider;
 import org.drools.planner.core.move.Move;
-import org.drools.planner.core.move.factory.MoveFactory;
+import org.drools.planner.core.move.factory.CachedMoveFactory;
 import org.drools.planner.core.solution.Solution;
 
-public class VariableMoveFactory implements MoveFactory {
-	public List<Move> createMoveList(Solution solution) {
+public class VariableMoveFactory extends CachedMoveFactory {
+	public List<Move> createCachedMoveList(Solution solution) {
 		ArrayList<Move> newmoves=new ArrayList<Move>();
 		
-		newmoves.add(new VariableMove(1, ((VariableSolution)solution).variables.get(0)));
-		newmoves.add(new VariableMove(-1, ((VariableSolution)solution).variables.get(0)));
+		for(int x=1; x<11; x++){
+			newmoves.add(new VariableMove(1*x, ((VariableSolution)solution).variables.get(0)));
+			newmoves.add(new VariableMove(-1*x, ((VariableSolution)solution).variables.get(0)));
+		}
 		
 		return newmoves;
-	}
-
-	public void setDecider(Decider decider) {
-		
-	}
-
-	public void beforeDeciding(StepScope stepScope) {
-		
-	}
-
-	public void solvingEnded(LocalSearchSolverScope localSearchSolverScope) {
-		
-	}
-
-	public void solvingStarted(LocalSearchSolverScope localSearchSolverScope) {
-		
-	}
-
-	public void stepDecided(StepScope stepScope) {
-		
-	}
-
-	public void stepTaken(StepScope stepScope) {
-		
 	}
 }
